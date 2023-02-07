9dba760f80f102415e68124a62553d718df2c304
jsg4hw <jc.gamble@kellogg.northwestern.edu>
2023-02-07 22:11:09 +0000

    populate companies

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources "tacos" #notifies server to prepare to receive the resources tacos
   resources "dice"
   resources "cards"
+  resources "companies"
 end
 
 # manifest of the resources that the requests the server is ready to "listen for"/receive
