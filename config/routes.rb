655ed3d2a42abd4f34c834bf88102a2e28a13e63
jsg4hw <jc.gamble@kellogg.northwestern.edu>
2023-02-07 22:00:36 +0000

    cards

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources "tacos" #notifies server to prepare to receive the resources tacos
   resources "dice"
+  resources "cards"
 end
 
 # manifest of the resources that the requests the server is ready to "listen for"/receive
