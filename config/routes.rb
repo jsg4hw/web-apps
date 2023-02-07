89fdd85d64b7371bd2b700155ab514dc575f2b61
jsg4hw <jc.gamble@kellogg.northwestern.edu>
2023-02-07 21:26:29 +0000

    created dice

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources "tacos" #notifies server to prepare to receive the resources tacos
+  resources "dice"
 end
 
 # manifest of the resources that the requests the server is ready to "listen for"/receive
